915a330b0b81dfcb1fc483e465ee1b3941cd5d2f
Alex Ferreira <alex.ferreira1796@gmail.com>
2021-11-29 17:50:48 -0300

    adicionando past tmp

--- backend/src/modules/documents/useCases/openDocument/OpenDocumentUseCase.ts
+++ backend/src/modules/documents/useCases/openDocument/OpenDocumentUseCase.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 class OpenDocumentUseCase {
   execute(link: string) {
     const address = path.dirname(require.main.filename).replace("src", "");
-    return `${address}tmp/${link}`;
+    return `${address}tmp//${link}`;
   }
 }
 
